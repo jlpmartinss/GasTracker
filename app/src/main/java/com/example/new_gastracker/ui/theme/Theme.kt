583470d2d914691c3138b95d701d99ed1cc2a9f4
jlpmartinss <jm8244936@gmail.com>
2024-09-06 12:13:45 +0100

    new colors added

--- app/src/main/java/com/example/new_gastracker/ui/theme/Theme.kt
+++ app/src/main/java/com/example/new_gastracker/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.new_gastracker.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -14,13 +13,17 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = Black,
+    surface = Black
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = Black,
+    surface = Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
